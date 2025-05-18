4712aa8ec0d5e4a46746f3bcb5f7ab170b1060af
James <super890421@gmail.com>
2025-05-18 23:58:11 +0800

    改用lua的方式來達到原子性，還沒做持久化

--- src/main/java/com/example/snapUp/controller/TicketController.java
+++ src/main/java/com/example/snapUp/controller/TicketController.java
@@ -4,10 +4,15 @@ import com.example.snapUp.dto.PurchaseRequest;
 import com.example.snapUp.entity.Ticket;
 import com.example.snapUp.repository.TicketRepository;
 
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.ClassPathResource;
 import org.springframework.data.redis.core.StringRedisTemplate;
+import org.springframework.data.redis.core.script.DefaultRedisScript;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.transaction.annotation.Transactional;
 import org.slf4j.Logger;
@@ -23,6 +28,8 @@ public class TicketController {
 
     @Autowired
     private StringRedisTemplate redisTemplate;
+
+    private static String luaPath = "lua/decr_ticket_stock.lua";
 
     @PostMapping("/reset")
     @Transactional
@@ -63,51 +70,46 @@ public class TicketController {
 
     @PostMapping("/purchase")
     @Transactional
-    public String purchaseTicket(@RequestBody PurchaseRequest request) {
-        try {
-            logger.info("Attempting to purchase ticket with request: {}", request);
-            Long ticketId = request.getTicketId();
-            int quantity = request.getQuantity() != null ? request.getQuantity() : 1;
+    public String purchaseTicket(@RequestBody PurchaseRequest request) throws IOException {
 
-            String redisKeyStock = "ticket_stock:" + ticketId;
-            logger.info("Check Redis stock for key: {}", redisKeyStock);
+        Long ticketId = request.getTicketId();
+        int quantity = request.getQuantity() != null ? request.getQuantity() : 1;
+        String redisKeyStock = "ticket_stock:" + ticketId;
 
-            String stockStr = redisTemplate.opsForValue().get(redisKeyStock);
-            int stock;
-
-            if (stockStr != null) {
-                stock = Integer.parseInt(stockStr);
-                logger.info("Redis stock: {}", stock);
-            } else {
-                Optional<Ticket> optionalTicket = ticketRepository.findById(request.getTicketId());
-                if (optionalTicket.isEmpty()) {
-                    return "找不到票券！";
-                }
-                stock = optionalTicket.get().getStock();
-                redisTemplate.opsForValue().set(redisKeyStock, String.valueOf(stock));
-            }
-
-            if (stock >= quantity) {
-                // 同步redis
-                redisTemplate.opsForValue().set(redisKeyStock, String.valueOf(stock - quantity));
-                // 寫回DB
-                Optional<Ticket> optionalTicket = ticketRepository.findById(request.getTicketId());
-                if (optionalTicket.isPresent()) {
-                    Ticket ticket = optionalTicket.get();
-                    ticket.setStock(ticket.getStock() - quantity);
-                    ticketRepository.save(ticket);
-                    logger.info("Ticket purchased successfully. Remaining stock: {}", ticket.getStock());
-                    return "購票成功！剩餘票券：" + ticket.getStock();
-                } else {
-                    return "找不到票券！";
-                }
-            } else {
-                logger.warn("Not enough tickets available. Requested: {}, Available: {}", quantity, stock);
-                return "票券不足！剩餘票券：" + stock;
-            }
-        } catch (Exception e) {
-            logger.error("Error purchasing ticket", e);
-            return "購票時發生錯誤：" + e.getMessage();
+        // 查詢 DB 中的初始票數
+        Optional<Ticket> optionalTicket = ticketRepository.findById(request.getTicketId());
+        if (optionalTicket.isEmpty()) {
+            return "找不到票券！";
         }
+        int initStock = optionalTicket.get().getStock();
+
+        // 執行lua
+        List<String> keys = List.of(redisKeyStock);
+        List<String> argv = List.of(String.valueOf(quantity), String.valueOf(initStock));
+        Long result = exeLua(keys, argv);
+
+        // 根據回傳結果處理邏輯
+        if (result == null) {
+            return "購票失敗：Redis 無回應";
+        } else if (result == -1) {
+            return "購票失敗：票券不足";
+        } else {
+            return "購票成功！剩餘票券：" + result;
+        }
+    }
+
+    private Long exeLua(List<String> keys, List<String> ARGV) throws IOException {
+        DefaultRedisScript<Long> script = new DefaultRedisScript<>();
+        ClassPathResource luaFile = new ClassPathResource(luaPath);
+        String luaScript = new String(luaFile.getInputStream().readAllBytes(), StandardCharsets.UTF_8);
+
+        script.setScriptText(luaScript);
+        script.setResultType(Long.class);
+
+        return redisTemplate.execute(
+                script,
+                keys,
+                ARGV.toArray()
+        );
     }
 }
---
+++ src/main/resources/lua/decr_ticket_stock.lua
@@ -0,0 +1,15 @@
+local stock = redis.call('GET', KEYS[1])
+
+--如果redis 沒有初始化
+if not stock then
+    stock = tonumber(ARGV[2])
+    redis.call('SET', KEYS[1], stock)
+end
+
+local quantity = tonumber(ARGV[1])
+
+if stock < quantity then
+    return -1
+end
+
+return redis.call('DECRBY', KEYS[1], quantity)
