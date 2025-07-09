905e5870f13fd8bc06fce2b9a42974c9d6f921cb
James <super890421@gmail.com>
2025-07-09 23:08:54 +0800

    feat(order): increase ticket stock when an order is cancelled

--- src/main/java/com/example/snapUp/service/OrderService.java
+++ src/main/java/com/example/snapUp/service/OrderService.java
@@ -1,21 +1,30 @@
 package com.example.snapUp.service;
 
 import com.example.snapUp.entity.Orders;
+import com.example.snapUp.entity.Ticket;
 import com.example.snapUp.repository.OrdersRepository;
+import com.example.snapUp.repository.TicketRepository;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.redisson.api.RedissonClient;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.annotation.Async;
 
 import java.util.Optional;
 
 @Service
+@EnableAsync
 public class OrderService {
     private static final Logger logger = LoggerFactory.getLogger(OrderService.class);
 
     @Autowired
     private OrdersRepository ordersRepository;
+
+    @Autowired
+    private TicketRepository ticketRepository;
 
     @Autowired
     private RedissonClient redissonClient;
@@ -37,6 +46,7 @@ public class OrderService {
         return 0;
     }
 
+    // TODO 目前沒用
     public int cancelByOrderId(String orderId) {
         Optional<Orders> order = ordersRepository.findUnpaidOrderByTicketId(orderId);
         if (order.isPresent()) {
@@ -63,11 +73,14 @@ public class OrderService {
         redissonClient.getScoredSortedSet("order:delay:queue").add(expireAt, orderId);
     }
 
+    @Async
     public void cancelOrderById(String orderId) {
         Optional<Orders> order = ordersRepository.findById(orderId);
         if (order.isPresent() && "0".equals(order.get().getStatus())) {
             Orders o = order.get();
             o.setStatus("2");
+            Ticket ticket = ticketRepository.findById(o.getTicketType()).get();
+            ticketRepository.updateByType(o.getTicketType(), ticket.getStock() + 1);
             ordersRepository.save(o);
         }
     }
